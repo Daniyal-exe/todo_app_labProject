51151c0b48d072db2ee41ce54724b76e81767f30
Daniyal Ahmed <daniyalsleeps@gmail.com>
2024-12-16 13:32:06 +0500

    connection made

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,9 @@
 const mysql = require('mysql2');
 require('dotenv').config({ path: '../.env' });
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const urlDB = `mysql://uxz0qggbhfc3dsim:Zl0rLiT3BuIFrX7zllhf@bajvsp7e9c0lg8a22dcp-mysql.services.clever-cloud.com:3306/bajvsp7e9c0lg8a22dcp`
+
+const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
     if (err) {
